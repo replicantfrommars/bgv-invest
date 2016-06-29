e572ec555e7e80369a4995478d4d1ea19a265ac9
replicantfrommars <YKANALOSH@Yevgens-MacBook-Pro.local>
2016-06-28 17:03:28 -0700

    This Should Work MVP3

--- main/models.py
+++ main/models.py
@@ -12,33 +12,14 @@ else:
 graphenedb_url = os.environ.get("GRAPHENEDB_URL")
 graph = ServiceRoot(graphenedb_url).graph
 
-def timestamp():
-    epoch = datetime.utcfromtimestamp(0)
-    now = datetime.now()
-    delta = now - epoch
-    return delta.total_seconds()
-
-def date():
-    return datetime.now().strftime('%Y-%m-%d')
-
-def get_todays_recent_posts():
-    query = """
-    MATCH (user:User)-[:PUBLISHED]->(post:Post)<-[:TAGGED]-(tag:Tag)
-    WHERE post.date = {today}
-    RETURN user.username AS username, post, COLLECT(tag.name) AS tags
-    ORDER BY post.timestamp DESC LIMIT 5
-    """
-
-    return graph.cypher.execute(query, today=date())
-
 class User:
     def __init__(self, username):
         self.username = username
-    
+
     def find(self):
         user = graph.find_one("User", "username", self.username)
         return user
-    
+
     def register(self, password):
         if not self.find():
             user = Node("User", username=self.username, password=bcrypt.encrypt(password))
@@ -46,7 +27,7 @@ class User:
             return True
         else:
             return False
-    
+
     def verify_password(self, password):
         user = self.find()
         if user:
@@ -72,7 +53,12 @@ class User:
             tag = graph.merge_one("Tag", "name", t)
             rel = Relationship(tag, "TAGGED", post)
             graph.create(rel)
-    
+
+    def like_post(self, post_id):
+        user = self.find()
+        post = graph.find_one("Post", "id", post_id)
+        graph.create_unique(Relationship(user, "LIKED", post))
+
     def get_recent_posts(self):
         query = """
         MATCH (user:User)-[:PUBLISHED]->(post:Post)<-[:TAGGED]-(tag:Tag)
@@ -82,3 +68,50 @@ class User:
         """
 
         return graph.cypher.execute(query, username=self.username)
+
+    def get_similar_users(self):
+        # Find three users who are most similar to the logged-in user
+        # based on tags they've both blogged about.
+        query = """
+        MATCH (you:User)-[:PUBLISHED]->(:Post)<-[:TAGGED]-(tag:Tag),
+              (they:User)-[:PUBLISHED]->(:Post)<-[:TAGGED]-(tag)
+        WHERE you.username = {username} AND you <> they
+        WITH they, COLLECT(DISTINCT tag.name) AS tags
+        ORDER BY SIZE(tags) DESC LIMIT 3
+        RETURN they.username AS similar_user, tags
+        """
+
+        return graph.cypher.execute(query, username=self.username)
+
+    def get_commonality_of_user(self, other):
+        # Find how many of the logged-in user's posts the other user
+        # has liked and which tags they've both blogged about.
+        query = """
+        MATCH (they:User {username: {they} })
+        MATCH (you:User {username: {you} })
+        OPTIONAL MATCH (they)-[:PUBLISHED]->(:Post)<-[:TAGGED]-(tag:Tag),
+                       (you)-[:PUBLISHED]->(:Post)<-[:TAGGED]-(tag)
+        RETURN SIZE((they)-[:LIKED]->(:Post)<-[:PUBLISHED]-(you)) AS likes,
+               COLLECT(DISTINCT tag.name) AS tags
+        """
+
+        return graph.cypher.execute(query, they=other.username, you=self.username)[0]
+
+def get_todays_recent_posts():
+    query = """
+    MATCH (user:User)-[:PUBLISHED]->(post:Post)<-[:TAGGED]-(tag:Tag)
+    WHERE post.date = {today}
+    RETURN user.username AS username, post, COLLECT(tag.name) AS tags
+    ORDER BY post.timestamp DESC LIMIT 5
+    """
+
+    return graph.cypher.execute(query, today=date())
+
+def timestamp():
+    epoch = datetime.utcfromtimestamp(0)
+    now = datetime.now()
+    delta = now - epoch
+    return delta.total_seconds()
+
+def date():
+    return datetime.now().strftime('%Y-%m-%d')
