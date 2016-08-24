86c7f28299750662e6a2c9ae8189750b6e464bf9
replicantfrommars <YKANALOSH@Yevgens-MacBook-Pro.local>
2016-08-24 13:05:33 +0300

    postgre

--- app/__init__.py
+++ app/__init__.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 app.config['DEBUG'] = False
 app.config['WTF_CSRF_ENABLED']=False
 app.config['SECRET_KEY'] = os.urandom(24)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite://'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://gwxkmmbzhvlitq:_IWTuiJFHAhJVsUYwmVJGdEN4q@ec2-54-227-245-222.compute-1.amazonaws.com:5432/damm4q15n6qaur'
 app.config['SECURITY_LOGIN_USER_TEMPLATE'] = 'security/login.html'
 
 from views import *
