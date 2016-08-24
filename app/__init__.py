2a75e32bb25dcb6f5f8ff2aaa2d4003e04a9c356
replicantfrommars <YKANALOSH@Yevgens-MacBook-Pro.local>
2016-08-24 22:12:55 +0300

    redirect

--- app/__init__.py
+++ app/__init__.py
@@ -8,6 +8,6 @@ app.config['DEBUG'] = False
 app.config['WTF_CSRF_ENABLED']=False
 app.config['SECRET_KEY'] = os.urandom(24)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://gwxkmmbzhvlitq:_IWTuiJFHAhJVsUYwmVJGdEN4q@ec2-54-227-245-222.compute-1.amazonaws.com:5432/damm4q15n6qaur'
-app.config['SECURITY_LOGIN_USER_TEMPLATE'] = 'security/login.html'
+app.config['SECURITY_LOGIN_USER_TEMPLATE'] = 'index.html'
 
 from views import *
