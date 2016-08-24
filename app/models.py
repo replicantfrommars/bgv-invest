0362e01bbda30871fb31f8b8025dc97a2b20519b
replicantfrommars <YKANALOSH@Yevgens-MacBook-Pro.local>
2016-08-24 12:20:58 +0300

    B testing Ann

--- app/models.py
+++ app/models.py
@@ -38,7 +38,7 @@ def create_user():
     user_datastore.create_user(email='Admin', password='PoIsNot8080', roles=['admin']) 
     user_datastore.create_user(email='yash@benhamouglobalventures.com', password='bgv_psd09235', roles=['admin']) 
     user_datastore.create_user(email='testAnuke', password='testAnuke', roles=['nonadmin']) 
-    user_datastore.create_user(email='testAnn', password='testAnn', roles=['']) 
+    user_datastore.create_user(email='testAnn', password='testAnn', roles=['nonadmin']) 
     db.session.commit()
 
 # Setup Flask-Security
